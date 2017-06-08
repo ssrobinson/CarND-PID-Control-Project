8e7296ab0a2107294740eebb1d2fac9199c3d477
Sean Robinson <sean.robinson36@gmail.com>
2017-06-08 13:41:43 -0600

    Add files via upload

---
+++ src/PID.cpp
@@ -0,0 +1,29 @@
+#include "PID.h"
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+}
+
+void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    i_error += cte;
+    p_error = cte;
+}
+
+double PID::TotalError() {
+  return i_error;
+}
